401023293712dd4394d5ba30704d5704ef9ea96d
Artur Carvalho <arturcarvalho0507@gmail.com>
2024-09-25 21:17:21 +0100

    added some structure to the repo

--- syncro_tools.c
+++
@@ -1,9 +0,0 @@
-#include "philo.h"
-
-// Spinlock until all threads are created and ready
-// Busy waiting
-void	ft_sync_threads(t_dinner *dinner)
-{
-	while (ft_get_bool(&dinner->mutex, &dinner->are_sync) == false)
-		;
-}
